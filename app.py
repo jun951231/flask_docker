f945f2649786c65ca5017f7882af3ab6803cfd71
jun951231 <jun951231@gmail.com>
2021-10-07 11:01:50 +0900

    Oct 07

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +9,6 @@ app = Flask(__name__)
 def hello():
     return "Hello World"
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=int(os.environ.get("PORT", 5000)))
--- basic/one_to_ten_sum.py
+++ basic/one_to_ten_sum.py
@@ -1,17 +1,29 @@
+from icecream import ic
+
 class OneToTenSum(object):
 
     def one_to_ten_sum_1(self):
-        # example 1
-        # 루프의 기본
+        # 예시 1
         sum = 0
         for i in range(1, 11):
             sum += i
-        print(sum)
+        return sum
 
 
     def one_to_ten_sum_2(self):
-        print(sum(i for i in range(1, 11)))
+        # 예시 2
+        val = sum(i for i in range(1, 11))
+        return val
 
 
     def one_to_ten_sum_3(self):
-        print(sum(range(1, 11)))
+        # 예시 3
+        val = sum(range(1, 11))
+        return val
+
+
+if __name__ == '__main__':
+    a = OneToTenSum()
+    ic(a.one_to_ten_sum_1())
+    ic(a.one_to_ten_sum_2())
+    ic(a.one_to_ten_sum_3())
